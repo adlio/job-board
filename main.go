45084b7397dbec774024e4b825d9ea4e992e3309
Aaron Longwell <aaron@culturefoundry.com>
2018-03-18 16:18:10 -0700

    Adds logging of requests.

--- main.go
+++ main.go
@@ -9,7 +9,11 @@ import (
 
 func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		// Render output for the request
 		fmt.Fprintf(w, "Hello, world at %v", time.Now())
+
+		// Log incoming requests
+		fmt.Printf("%s %s\n", r.Method, r.URL)
 	})
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
